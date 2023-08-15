8f0eb2885193caba6011519a6484de479f1fadc4
Faciqui Doumbia <faciquidoumbia@gmail.com>
2023-08-15 20:01:01 +0200

    displaying image from database

--- src/main/java/com/example/recette/controllers/ImageController.java
+++ src/main/java/com/example/recette/controllers/ImageController.java
@@ -1,9 +1,9 @@
 package com.example.recette.controllers;
 
 import com.example.recette.command.RecipeCommand;
-import com.example.recette.domain.Recipe;
 import com.example.recette.services.ImageService;
 import com.example.recette.services.RecipeService;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +11,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Controller
 public class ImageController {
@@ -31,5 +36,19 @@ public class ImageController {
     public String handleImagePost(@PathVariable String id, @RequestParam("imageFile")MultipartFile file) {
         imageService.saveImageFile(Long.valueOf(id), file);
         return "redirect:/recipe/" + id + "/show";
+    }
+
+    @GetMapping("recipe/{id}/recipeImage")
+    public void renderImageFromDB(@PathVariable String id, HttpServletResponse response) throws IOException {
+        RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(id));
+        byte[] byteArray = new byte[recipeCommand.getImage().length];
+        int i = 0;
+        for (Byte b: recipeCommand.getImage()) {
+            byteArray[i++] = b;
+        }
+
+        response.setContentType("image/jpeg");
+        InputStream is = new ByteArrayInputStream(byteArray);
+        IOUtils.copy(is, response.getOutputStream());
     }
 }
--- src/test/java/com/example/recette/services/ImageServiceImplTest.java
+++ src/test/java/com/example/recette/services/ImageServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.example.recette.services;
 
+import com.example.recette.command.RecipeCommand;
 import com.example.recette.domain.Recipe;
 import com.example.recette.repositories.RecipeRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,7 +8,9 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -22,6 +25,7 @@ class ImageServiceImplTest {
     @Mock
     RecipeRepository recipeRepository;
 
+    RecipeService recipeService;
     ImageService imageService;
 
     @BeforeEach
@@ -41,7 +45,7 @@ class ImageServiceImplTest {
         recipe.setId(id);
         Optional<Recipe> recipeOptional = Optional.of(recipe);
 
-        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+        when(recipeRepository.findById(anyLong())).thenReturn(Optional.of(recipe));
 
         ArgumentCaptor<Recipe> argumentCaptor = ArgumentCaptor.forClass(Recipe.class);
 
@@ -53,4 +57,6 @@ class ImageServiceImplTest {
         Recipe savedRecipe = argumentCaptor.getValue();
         assertEquals(multipartFile.getBytes().length, savedRecipe.getImages().length );
     }
+
+
 }
